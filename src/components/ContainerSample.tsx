ad4407e0c0111c35e35ad301dff62fedad8356ad
Aaron Song <aaron@kmong.com>
2024-01-08 19:57:26 +0900

    fix: props에 type 사용하기

--- src/components/ContainerSample.tsx
+++ src/components/ContainerSample.tsx
@@ -1,4 +1,10 @@
-const Container = (props: { title: string; children: React.ReactElement }) => {
+type ContainerProps = {
+    title: string
+    children: React.ReactNode
+}
+
+
+const Container = (props: ContainerProps): JSX.Element => {
     const {title, children} = props
 
     return (
@@ -9,3 +15,13 @@ const Container = (props: { title: string; children: React.ReactElement }) => {
         </div>
     )
 }
+
+const Parent = (): JSX.Element => {
+    return (
+        <Container title="Hello">
+            <p>이 부분이 배경색으로 둘러싸여 있습니다</p>
+        </Container>
+    )
+}
+
+export default Parent
