7ac2738e668a8b73f64c02106bbbafcbfa2d4551
Aaron Song <aaron@kmong.com>
2024-01-08 20:05:09 +0900

    feat: Context를 사용한 데이터 전달

---
+++ src/components/ContextSample.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+const TitleContext = React.createContext('')
+
+const Title = () => {
+    return (
+        <TitleContext.Consumer>
+            {(title) => {
+                return  <h1>{title}</h1>
+            }}
+        </TitleContext.Consumer>
+    )
+}
+
+const Header = () => {
+    return (
+        <h1>
+            <Title />
+        </h1>
+    )
+}
+
+const Page = () => {
+    const title = 'React Hook'
+
+    return (
+        <TitleContext.Provider value = {title}>
+            <Header />
+        </TitleContext.Provider>
+    )
+}
+
+export default Page
