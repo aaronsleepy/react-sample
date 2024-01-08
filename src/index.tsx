97a96fb0a307f73d1bce08820532ef78aba638a7
Aaron Song <aaron@kmong.com>
2024-01-08 20:11:52 +0900

    feat: useState 예제

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './components/Hello';
+import Counter from './components/HookUseStateSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <Hello />
+    <Counter initialValue={1} />
   </React.StrictMode>
 );
 
